116377d951ff1191353cf2c3061654ab4ac4df6d
Cory Pierson <corypierson@corys-air.myfiosgateway.com>
2024-06-23 13:21:19 -0400

    Setup authentication. Set route private.

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,17 @@
 import express, { Express } from 'express'
 
 import dotenv from 'dotenv'
+import { auth } from 'express-oauth2-jwt-bearer'
 import routes from './routes'
 
 dotenv.config()
+
+// Auth0 configuration
+const jwtCheck = auth({
+  audience: process.env.AUTH0_AUDIENCE,
+  issuerBaseURL: process.env.AUTH0_ISSUER_BASE_URL,
+  tokenSigningAlg: 'RS256',
+})
 
 const app: Express = express()
 const port = process.env.PORT || 3000
@@ -11,7 +19,12 @@ const port = process.env.PORT || 3000
 // Middleware
 app.use(express.json())
 
-app.use('/api', routes)
+// Public route
+app.get('/api/public', (req, res) => {
+  res.json({ message: 'This is a public endpoint' })
+})
+
+app.use('/api', jwtCheck, routes)
 
 app.listen(port, () => {
   console.log(`[server]: Server is running at http://localhost:${port}`)
